96dfc8941d32a789176f566b6a74d67952c099fe
Niki Brown <hi@nikibrown.com>
2020-04-03 18:13:01 -0700

    add colors and plate styles

--- src/App.vue
+++ src/App.vue
@@ -204,42 +204,42 @@ export default {
 
             largePlates: [
                 {
-                    bsClasses: "btn btn-plate btn-danger weight-lg pound-weight-55 kilo-weight-25",
+                    bsClasses: "btn btn-plate weight-lg pound-weight-55 kilo-weight-25",
                     poundWeight: 55,
                     kiloWeight: 25,
                     plateCount: 0,
                     isOnBarbell: false
                 },
                 {
-                    bsClasses: "btn btn-plate btn-primary weight-lg pound-weight-45 kilo-weight-20",
+                    bsClasses: "btn btn-plate weight-lg pound-weight-45 kilo-weight-20",
                     poundWeight: 45,
                     kiloWeight: 20,
                     plateCount: 0,
                     isOnBarbell: false
                 },
                 {
-                    bsClasses: "btn btn-plate btn-warning weight-lg pound-weight-35 kilo-weight-15",
+                    bsClasses: "btn btn-plate weight-lg pound-weight-35 kilo-weight-15",
                     poundWeight: 35,
                     kiloWeight: 15,
                     plateCount: 0,
                     isOnBarbell: false
                 },
                 {
-                    bsClasses: "btn btn-plate btn-success weight-lg pound-weight-25 kilo-weight-10",
+                    bsClasses: "btn btn-plate weight-lg pound-weight-25 kilo-weight-10",
                     poundWeight: 25,
                     kiloWeight: 10,
                     plateCount: 0,
                     isOnBarbell: false
                 },
                 {
-                    bsClasses: "btn btn-plate btn-dark weight-lg pound-weight-15 kilo-weight-hide",
+                    bsClasses: "btn btn-plate weight-lg pound-weight-15 kilo-weight-hide",
                     poundWeight: 15,
                     kiloWeight: 0,
                     plateCount: 0,
                     isOnBarbell: false
                 },
                 {
-                    bsClasses: "btn btn-plate btn-dark weight-lg pound-weight-10 kilo-weight-hide",
+                    bsClasses: "btn btn-plate weight-lg pound-weight-10 kilo-weight-hide",
                     poundWeight: 10,
                     kiloWeight: 0,
                     plateCount: 0,
@@ -249,42 +249,42 @@ export default {
 
             smallPlates: [
                 {
-                    bsClasses: "btn btn-plate btn-dark weight-sm",
+                    bsClasses: "btn btn-plate weight-sm",
                     poundWeight: 5,
                     kiloWeight: 5,
                     plateCount: 0,
                     isOnBarbell: false
                 },
                 {
-                    bsClasses: "btn btn-plate btn-dark weight-sm",
+                    bsClasses: "btn btn-plate weight-sm",
                     poundWeight: 2.5,
                     kiloWeight: 2.5,
                     plateCount: 0,
                     isOnBarbell: false
                 },
                 {
-                    bsClasses: "btn btn-plate btn-dark weight-sm",
+                    bsClasses: "btn btn-plate weight-sm",
                     poundWeight: 1,
                     kiloWeight: 2,
                     plateCount: 0,
                     isOnBarbell: false
                 },
                 {
-                    bsClasses: "btn btn-plate btn-dark weight-sm",
+                    bsClasses: "btn btn-plate weight-sm",
                     poundWeight: .75,
                     kiloWeight: 1.5,
                     plateCount: 0,
                     isOnBarbell: false
                 },
                 {
-                    bsClasses: "btn btn-plate btn-dark weight-sm",
+                    bsClasses: "btn btn-plate weight-sm",
                     poundWeight: .5,
                     kiloWeight: 1,
                     plateCount: 0,
                     isOnBarbell: false
                 },
                 {
-                    bsClasses: "btn btn-plate btn-dark weight-sm",
+                    bsClasses: "btn btn-plate weight-sm",
                     poundWeight: .25,
                     kiloWeight: .5,
                     plateCount: 0,
@@ -432,6 +432,8 @@ export default {
 <style lang="scss">
 @import url('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
 @import url('https://fonts.googleapis.com/css2?family=Heebo:wght@500;900&display=swap');
+@import "./styles/colors.scss";
+
 
 
 body {
--- src/components/Plate.vue
+++ src/components/Plate.vue
@@ -55,24 +55,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../styles/colors.scss";
+
+.btn-plate {
+  color: $white;
+  &:hover {
+    color: $white;
+  }
+}
+
 .weight-lg {
+  border: 5px solid;
   min-width: 100px;
   min-height: 100px;
 }
 
 .weight-sm {
-  min-width: 60px;
-  min-height: 60px;
+  
+  background-color: $darkGray;
+  border: 3px solid;
+  border-color: darken($darkGray, 10%);
+  min-width: 70px;
+  min-height: 70px;
+  &:hover {
+    background-color: darken($darkGray, 10%);
+  }
 }
 
 
 @media screen and (max-width: 900px) {
     .weight-lg,
     .weight-sm {
-        font-size: 14px;
-        min-width: 60px;
-        min-height: 60px;
-        margin: 3px;
+      border-width: 3px;
+      font-size: 14px;
+      min-width: 70px;
+      min-height: 70px;
+      margin: 3px;
     }
 }
 
@@ -93,4 +111,47 @@ export default {
 .weight-label {
   font-size: 10px;
 }
+
+.pound-weight-55 {
+  background-color: $red;
+  border-color: darken($red, 10%);
+  &:hover {
+    background-color: darken($red, 10%);
+  }
+}
+
+.pound-weight-45 {
+  background-color: $blue;
+  border-color: darken($blue, 10%);
+  &:hover {
+    background-color: darken($blue, 10%);
+  }
+}
+
+.pound-weight-35 {
+  background-color: $yellow;
+  border-color: darken($yellow, 10%);
+  &:hover {
+    background-color: darken($yellow, 10%);
+  }
+}
+
+.pound-weight-25 {
+  background-color: $green;
+  border-color: darken($green, 10%);
+  &:hover {
+    background-color: darken($green, 10%);
+  }
+}
+
+.pound-weight-15,
+.pound-weight-10 {
+  background-color: $darkGray;
+  border-color: darken($darkGray, 10%);
+  &:hover {
+    background-color: darken($darkGray, 10%);
+  }
+}
+
+
 </style>
